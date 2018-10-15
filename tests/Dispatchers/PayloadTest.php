e2b2159d7ea95525b6ddfc17306c1bcc9de9d474
Dirk Gustke <dirk.gustke@verticalmedia.com>
2018-10-16 01:08:35 +0200

    master - test payload dispatcher

---
+++ tests/Dispatchers/PayloadTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Resque\Dispatchers;
+use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
+use Psr\EventDispatcher\TaskProcessorInterface;
+use Resque\Traits\Payloadable;
+use Resque\Interfaces\PayloadableTaskInterface;
+
+class PayloadTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->serviceLocator = $this->getServiceLocatorMock();
+        $this->processor = $this->getTaskProcessorMock();
+        $this->payloadable = $this->getPayloadableTaskMock();
+        $this->dispatcher = new Payload($this->serviceLocator, $this->processor);
+    }
+
+    public function tearDown()
+    {
+    }
+
+    public function testDispatchShouldDispatchTheTaskWithPayloadAndReturnTheProcessedPayload()
+    {
+        $className = 'SomeClass';
+        $payload = ['some_key' => 'some_value'];
+        $processedPayload = ['some_new_key' => 'some_other_value'];
+        $this->serviceLocator->expects($this->once())
+            ->method('get')
+            ->with($className)
+            ->willReturn($this->payloadable)
+        ;
+
+        $this->processor->expects($this->once())
+            ->method('process')
+            ->with($this->payloadable)
+            ->willReturn($this->payloadable)
+        ;
+
+        $this->payloadable->expects($this->once())
+            ->method('setPayload')
+            ->with($payload)
+        ;
+
+        $this->payloadable->expects($this->once())
+            ->method('getPayload')
+            ->willReturn($processedPayload)
+        ;
+
+        $payloadInQuestion = $this->dispatcher->dispatch($className, $payload);
+
+        $this->assertEquals($processedPayload, $payloadInQuestion);
+    }
+
+    private function getServiceLocatorMock()
+    {
+        return $this->getMockBuilder(ContainerInterface::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['get', 'has'])
+            ->getMock()
+        ;
+    }
+
+    private function getTaskProcessorMock()
+    {
+        return $this->getMockBuilder(TaskProcessorInterface::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['process'])
+            ->getMock()
+        ;
+    }
+
+    private function getPayloadableTaskMock()
+    {
+        return $this->getMockBuilder(PayloadableTaskInterface::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['getPayload', 'setPayload'])
+            ->getMock()
+        ;
+    }
+}
